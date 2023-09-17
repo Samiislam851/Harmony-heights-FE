ebb7ca28049ca7ea10f20e59e8958bb6dc1b5b54
siam <samisiam851@gmail.com>
2023-09-17 17:08:30 +0600

    intructor list public

--- src/Pages/InstructorListPages/InstructorListPages.jsx
+++ src/Pages/InstructorListPages/InstructorListPages.jsx
@@ -18,8 +18,8 @@ const InstructorListPages = ({setTitle }) => {
     const [progresssending, setprogresssending] = useState(false)
    
     useEffect(() => {
-        if (loading && user) {
-          setTitle("My Classes")
+        if (loading ) {
+          setTitle("Instructors")
             axios.get(`/instructor/all`)
                 .then(response => {
                     let data = response.data
@@ -27,7 +27,7 @@ const InstructorListPages = ({setTitle }) => {
                     setRenderData(data)
                     setLoading(false)
 
-                })
+                }).catch(err => console.log('the error',err))
         }
     }, []);
     
@@ -41,7 +41,7 @@ const InstructorListPages = ({setTitle }) => {
 
 <div class="bg-white">
   <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="text-xl font-bold text-gray-900">Instructor List</h2>
+    <h2 class="md:text-6xl text-center pb-20 font-semibold text-gray-600">Instructor List</h2>
     <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
     {loading ? <>
                 <Spinner />
